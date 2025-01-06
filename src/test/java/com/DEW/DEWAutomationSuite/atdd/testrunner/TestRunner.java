e8c0c38c30e05bbd729edd782e25a3d43c7df93e
SSingh2 <SSingh2@DEW214542.env.sa.gov.au>
2025-01-06 21:27:37 +1030

    commit

--- src/test/java/com/DEW/DEWAutomationSuite/atdd/testrunner/TestRunner.java
+++ src/test/java/com/DEW/DEWAutomationSuite/atdd/testrunner/TestRunner.java
@@ -13,7 +13,7 @@ package com.DEW.DEWAutomationSuite.atdd.testrunner;
 	dryRun = false,
 	plugin = { "pretty","html:target/cucumber-reports.html", "json:target/cucumber-reports.json" }, 
 	monochrome = true,
-	tags = "@Regression")
+	tags = "@WellPermit")
 	 
 	public class TestRunner {
 		
