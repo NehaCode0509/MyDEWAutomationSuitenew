3b87bd9320722c28a78fd41cf3726c79b2b0d0bf
NehaCode0509 <nehasinha44@gmail.com>
2025-01-14 13:20:55 +1030

    commit

--- src/test/java/com/DEW/DEWAutomationSuite/atdd/testrunner/TestRunner.java
+++ src/test/java/com/DEW/DEWAutomationSuite/atdd/testrunner/TestRunner.java
@@ -14,7 +14,7 @@ package com.DEW.DEWAutomationSuite.atdd.testrunner;
 	dryRun = false,
 	plugin = { "pretty","html:target/cucumber-reports.html", "json:target/cucumber-reports.json" }, 
 	monochrome = true,
-	tags = " @Regression")
+	tags = "@GoogleTest ")
 	 
 	public class TestRunner {
 		
